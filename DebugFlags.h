2d30726f660b87b75ee864f43844dc248b1d3e20
Ralph <rp15@rpphoto.com>
2026-06-03 15:54:23 -0700

    Restore debug flags to DebugFlags.h

--- DebugFlags.h
+++ DebugFlags.h
@@ -11,17 +11,18 @@
 
 /*****************      DEBUGGING FLAGS      ******************/
 
-bool _isDEBUG_BypassGPS = true;					// Bypass gps syncing.
-bool _isDEBUG_BypassWifi = false;				// Bypass WiFi connect.	 XXX FAILS!!!
-bool _isDEBUG_BypassSDCard = true;				// Bypass SD card.
-bool _isDEBUG_ListLittleFS = false;				// List contents of LittleFS.
-bool _isDEBUG_BypassWebServer = false;			// Bypass Web Server.
-bool _isDEBUG_run_test_in_setup = false;		// Run only test code inserted in Setup.
-bool _isDEBUG_run_test_in_loop = false;			// Run test code inserted in Loop.
-bool _isDEBUG_addDummyDataLists = false;		// Add dummy data.
-bool _isDEBUG_simulateSensorReadings = false;	// Add dummy sensor reading values.
-bool _isDEBUG_simulateWindReadings = false;		// Add dummy wind reading values.
-bool _isDEBUG_AddDelayInLoop = false;			// Add delay in loop.
-const int _LOOP_DELAY_DEBUG_ms = 100;			// Debug delay in loop, msec.
+constexpr bool isDEBUG_BypassGPS = true;				// Bypass gps syncing.
+constexpr bool isDEBUG_BypassWifi = false;				// Bypass WiFi connect.	 XXX FAILS!!!
+constexpr bool isDEBUG_BypassSDCard = false;			// Bypass SD card.
+constexpr bool isDEBUG_ListLittleFS = true;				// List contents of LittleFS.
+constexpr bool isDEBUG_BypassWebServer = false;			// Bypass Web Server.
+constexpr bool isDEBUG_run_test_in_setup = false;		// Run only test code inserted in Setup.
+constexpr bool isDEBUG_run_test_in_loop = false;		// Run test code inserted in Loop.
+constexpr bool isDEBUG_addDummyDataLists = false;		// Add dummy data.
+constexpr bool isDEBUG_simulateSensorReadings = false;	// Add dummy sensor reading values.
+constexpr bool isDEBUG_simulateWindReadings = false;	// Add dummy wind reading values.
+constexpr bool isDEBUG_AddDelayInLoop = false;			// Add delay in loop.
+constexpr int LOOP_DELAY_DEBUG_ms = 100;				// Debug delay in loop, msec.
+
 
 #endif
